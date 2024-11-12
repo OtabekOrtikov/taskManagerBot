e99066640227c3d46da03bd1b68294298daa86a6
Otabek Ortikov <Otabek_Ortikov@student.itpu.uz>
2024-11-12 18:02:29 +0500

    changed config

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-API_TOKEN = '7433412474:AAE0EYgcvcV56jiBodkx4gvjxQLLjwdwtPo'
+# test bot
+# API_TOKEN = '7433412474:AAE0EYgcvcV56jiBodkx4gvjxQLLjwdwtPo'
+API_TOKEN = '7314896845:AAE4AXQdXTaeaN-BfegtS2EbzHKi7U68OOw'
 
 BOT_USERNAME = 'ManageTaskBot'
 
-# DATABASE_URL = 'postgresql://ixlosfcu_taskManagerBot:h2i-5wn-5Ah-Ufh@localhost:5432/ixlosfcu_taskManager'
-DATABASE_URL = 'postgresql://postgres:01042006Otabek!@localhost:5432/TaskManagerBot'
+DATABASE_URL = 'postgresql://ixlosfcu_taskManagerBot:h2i-5wn-5Ah-Ufh@localhost:5432/ixlosfcu_taskManager'
+# DATABASE_URL = 'postgresql://postgres:01042006Otabek!@localhost:5432/TaskManagerBot'
 
 drop_table = 'drop table company, department, project, task, user_m2m_project, users'
 
