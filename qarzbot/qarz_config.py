39b49068a1de4280d3ecd42710b0a3521b1b17fc
Otabek Ortikov <Otabek_Ortikov@student.itpu.uz>
2025-03-07 23:36:09 +0500

    fixed config for qarz

--- qarzbot/qarz_config.py
+++ qarzbot/qarz_config.py
@@ -3,7 +3,7 @@ API_TOKEN = "8063580642:AAG9GN0y_LZ6Np4eM0dDD4kFKXGZqZc-pO4"
 BOT_USERNAME = "ManageDebtBot"
 
 # hosting
-DATABASE_URL = 'postgresql://ixlosfcu_otabek:h2i-5wn-5Ah-Ufh@localhost:5432/ixlosfcu_qarzbot'
+DATABASE_URL = 'postgresql://ixlosfcu_taskManagerBot:h2i-5wn-5Ah-Ufh@localhost:5432/ixlosfcu_qarzbot'
 
 # DATABASE_URL = "postgresql://postgres:1234@localhost:5432/QarzBot"
 
