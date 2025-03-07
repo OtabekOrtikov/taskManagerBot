4667ddef39ee8d0a68fc189bfb5bcff87571680a
Otabek Ortikov <Otabek_Ortikov@student.itpu.uz>
2025-03-07 23:19:36 +0500

    fixed version of requirements

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_TOKEN = '7314896845:AAE4AXQdXTaeaN-BfegtS2EbzHKi7U68OOw'
 BOT_USERNAME = 'ManageTaskBot'
 
 DATABASE_URL = 'postgresql://ixlosfcu_taskManagerBot:h2i-5wn-5Ah-Ufh@localhost:5432/ixlosfcu_taskManager'
-# DATABASE_URL = 'postgresql://postgres:01042006Otabek!@localhost:5432/TaskManagerBot'
+# DATABASE_URL = 'postgresql://postgres:1234@localhost:5432/TaskManagerBot'
 
 drop_table = 'drop table company, department, project, task, user_m2m_project, users'
 
